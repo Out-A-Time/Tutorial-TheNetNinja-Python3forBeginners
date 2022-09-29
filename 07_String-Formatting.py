f114035136210e6ec763282acee6f3f516796f45
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2022-09-29 13:25:25 +0100

    Lesson07 - done

---
+++ 07_String-Formatting.py
@@ -0,0 +1,11 @@
+num1 = 3.14567
+num2 = 10.34876
+
+#FORMAT METHOD 1:
+print('Format Method 1: num1 is', num1, 'and num2 is', num2)
+
+#FORMAT METHOD 2:
+print('Format Method 2: num1 is {0:.3f} and num2 is {1:.3f}'.format(num1, num2))
+
+#FORMAT METHOD 3: f-String
+print(f'Format Method 3 (f-String): num1 is {num1:.4f} and num2 is {num2:.4f}')
